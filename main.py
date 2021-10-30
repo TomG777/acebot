44103451577d424a2ea5674f00ef43eb14ad04b1
Tom <GitKraken@tom.al>
2021-10-30 16:34:57 +0200

    Add basic support for starting a stream

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import base64
+
 import discord
 import asyncio
 import utils
+import requests
 
 client = discord.Client()
 config = utils.load_config()
 prefix = config.get("prefix", ";")
 prefix_length = len(prefix)
 commands = {}
+
+engines = []
 
 
 def cmd(name, help="", mod=False, admin=False, master=False):
@@ -84,10 +89,92 @@ async def command_help(message, arguments):
     return start + result + end
 
 
+@cmd("start", "Start a stream", master=True)
+async def command_run(message, arguments):
+    title, url = arguments.split(None, 1)
+    words = utils.get_words(arguments)
+    if len(words) < 2:
+        return "Missing arguments: <title> <url> [number] [bitrate]"
+
+    command = utils.build_command(*words[:4])
+    print(command)
+    proc = await asyncio.create_subprocess_shell(
+        command,
+        stdin=asyncio.subprocess.PIPE,
+        stdout=asyncio.subprocess.PIPE)
+    proc.stdin.write(b'Hello\n')
+    data = await proc.stdout.readline()
+    print(data.decode().strip())
+    data = {
+        "proc": proc,
+        "title": words[0],
+        "url": words[1],
+    }
+    if len(words) >= 3:
+        data["number"] = words[2]
+    else:
+        data["number"] = 1
+    engines.append(data)
+    return len(engines)
+
+
+@cmd("stop", "Stop a running stream process", master=True)
+async def command_stop(message, arguments):
+    if arguments:
+        number = utils.get_words(arguments)[0]
+    else:
+        number = 0
+    data = engines[number]
+    proc = data.get("proc")
+    data = await proc.stdout.read()
+    print(data.decode().strip())
+    proc.terminate()
+    return str(data)[:1950]
+
+
+@cmd("read", "Read process output", master=True)
+async def command_stop(message, arguments):
+    if arguments:
+        number = utils.get_words(arguments)[0]
+    else:
+        number = 0
+    data = engines[int(number)]
+    proc = data.get("proc")
+    data = await proc.stdout.read()
+    print(data.decode().strip())
+    return str(data.decode())[:1950]
+
+
+@cmd("count", "Get the count of streams", mod=True)
+async def command_count(**_):
+    return len(engines)
+
+
+@cmd("get_id", "Get the content_id for a stream", mod=True)
+async def get_id(message, arguments):
+    if arguments:
+        number = int(utils.get_words(arguments)[0])
+    else:
+        number = 0
+    if number < len(engines):
+        data = engines[int(number)]
+        num = data["number"]
+    else:
+        num = number
+    dir = config.get("publishdir", "/var/www/html/stream")
+    file = dir + "/live" + str(num) + ".acelive"
+    with open(file, "rb") as f:
+        filedata = base64.b64encode(f.read())
+
+    r = requests.post("http://api.torrentstream.net/upload/raw", data=filedata)
+    print(r)
+    return r.text
+
+
 @cmd("commands", "Get a list of commands")
 async def command_commands(message, arguments):
     cmds = []
-    for argument, command in commands.items():
+    for argument, details in commands.items():
         if details["mod"] and not utils.check_mod(message.author):
             continue
         if details["admin"] and not utils.check_admin(message.author):
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import json
 import discord
+from more_itertools import peekable
 
 
 def load_config():
@@ -12,6 +13,130 @@ def load_config():
 
 
 config = load_config()
+
+
+def get_word(iterator: iter, multi=False):
+    word = []
+    letter = next(iterator)
+    if multi and letter == '"':
+        for letter in iterator:
+            if letter == "\\":
+                if iterator.peek(False) is False:
+                    return "".join(word)
+                next_letter = next(iterator)
+                if next_letter == '"':
+                    word.append(next_letter)
+                else:
+                    word.append(letter + next_letter)
+            if letter == '"':
+                return "".join(word)
+            word += letter
+        return "".join(word)
+    word.append(letter)
+    for letter in iterator:
+        if letter == " " or letter == "\n":
+            return "".join(word)
+        word.append(letter)
+    return "".join(word)
+
+
+def get_words(text: str):
+    words = []
+    if text == "":
+        return []
+    iterator = peekable(text)
+    while iterator.peek(False) is not False:
+        while iterator.peek() in [" ", "\n"]:
+            next(iterator)
+        words.append(get_word(iterator, True))
+    return words
+
+
+def argument_parser(text: str):
+    if text == "":
+        return [], {}, ""
+    flags = []
+    parameters = {}
+    iterator = peekable(text)
+    text = ""
+
+    while iterator.peek(False) is not False:
+        word = []
+        next_letter = iterator.peek()
+        if next_letter == "-":
+            _ = next(iterator)
+            next_letter = iterator.peek(None)
+            if next_letter is None:
+                text += "-"
+                break
+            if next_letter == "-":
+                _ = next(iterator)  # Get rid if the character
+                if iterator.peek(False) is not False:
+                    parm = get_word(iterator)
+                    if iterator.peek(False) is not False:
+                        value = get_word(iterator, True)
+                    else:
+                        value = None
+                    parameters[parm] = value
+                else:
+                    text += "--"
+                    break
+            else:
+                flag = get_word(iterator)
+                flags.append(flag)
+
+        else:
+            text += "".join([l for l in iterator])
+            break
+
+    return flags, parameters, text
+
+
+def get_ip():
+    ip = config.get("hostip")
+    if ip:
+        return ip
+    import requests
+    return requests.get("http://ipecho.net/plain").text
+
+
+def build_command(title, url, number=1, bitrate=600000):
+    #  --live-cache-type memory
+    command_parts = []
+    command_parts.append(config.get("engine", "/opt/acestream/acestreamengine"))
+    command_parts.append("--stream-source")
+    command_parts.append("--title '" + title + "'")
+    command_parts.append("--source '" + url + "'")
+    command_parts.append("--name live" + str(number))
+    command_parts.append("--port " + str(int(config.get("portstart", 888)) + int(number)))
+    command_parts.append("--host " + get_ip())
+    command_parts.append("--publish-dir '" + config.get("publishdir", "/var/www/html/stream") + "'")
+    command_parts.append(
+        "--cache-dir '" + config.get("publishdir", "/var/www/html/stream") + "/" + config.get("cachedir",
+                                                                                              "cache") + "'")
+    command_parts.append("--bitrate " + str(config.get("bitrate", bitrate)))
+    command_parts.append("--quality 'HD'")
+    command_parts.append("--category " + config.get("category"))
+    command_parts.append("--max-upload-slots " + str(config.get("maxslots", 100)))
+    command_parts.append("--max-peers " + str(config.get("maxpeers", 100)))
+
+    for tracker in config.get("trackers", []):
+        command_parts.append("--tracker " + tracker)
+    if config.get("permanent", True):
+        command_parts.append("--permanent")
+    if config.get("remoteaccess", False):
+        if config.get("remotetoken", False):
+            command_parts.append("--service-remote-access")
+            command_parts.append("--service-access-token " + config.get("remotetoken"))
+        else:
+            import random
+            import string
+            token = [random.choice(string.ascii_letters) for _ in range(16)]
+            print("Random access token: " + token)
+            command_parts.append("--service-remote-access")
+            command_parts.append("--service-access-token " + token)
+
+    return " ".join(command_parts)
 
 
 def check_mod(user: discord.User):
